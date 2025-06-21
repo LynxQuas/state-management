6a3bb23ce6c50a3a479a47396f75943374048d07
La Pyae Hmue Aug <lapyaehmueaug@192.168.5.110>
2025-06-21 15:17:09 +0700

    fix: login error state

--- src/page/login-page.jsx
+++ src/page/login-page.jsx
@@ -3,10 +3,15 @@ import useAuth from "../context/auth-context";
 
 export default function LoginPage() {
     const [username, setUsername] = useState("");
+    const [isError, setIsError] = useState(false);
     const { handleLogin } = useAuth();
 
     const handleSubmit = (e) => {
         e.preventDefault();
+        if (!username) {
+            setIsError(true);
+            return;
+        }
         handleLogin(username);
         setUsername("");
     };
@@ -19,13 +24,26 @@ export default function LoginPage() {
                 className="flex justify-center items-center h-[20rem]"
             >
                 <div className="flex flex-col gap-10 w-full px-4 md:w-[25rem] rounded-md">
-                    <input
-                        type="text"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
-                        placeholder="Username"
-                        className="p-4 w-full outline-none rounded-md border"
-                    />
+                    <div className="flex flex-col gap-2">
+                        <input
+                            type="text"
+                            value={username}
+                            onChange={(e) => {
+                                setUsername(e.target.value);
+                                setIsError(false);
+                            }}
+                            placeholder="Username"
+                            className={`p-4 w-full outline-none rounded-md border ${
+                                isError && "border-red-500"
+                            }`}
+                        />
+
+                        {isError && (
+                            <span className="text-red-500">
+                                Username is required.
+                            </span>
+                        )}
+                    </div>
 
                     <button className="bg-violet-500 font-bold shadow-sm text-white p-4 rounded-md">
                         Login
